694b11ed5f75319e40dc5a8a53225e83d2dffc5b
onsha <onsha.bogdan@gmail.com>
2020-03-07 18:05:00 +0200

    Fixed error in spiders/__pycache__/paginiaurii_spider

--- paginiaurii/paginiaurii/spiders/paginiaurii_spider.py
+++ paginiaurii/paginiaurii/spiders/paginiaurii_spider.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import re
+import os
 import logging
 import string
+import time
+import json
 
 import scrapy
+
+from export import export
+from send_email import send_email
 
 
 def text_to_query(s):
@@ -20,15 +26,87 @@ def get_start_urls(query, location):
         return [f'https://www.paginiaurii.ro/cauta/{text_to_query(query)}/']
 
 
+def create_config(search_query, search_location, output_file_format):
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    config.update(
+        {
+            'search_query': search_query,
+            'search_location': search_location,
+            'output_file_format': output_file_format
+        }
+    )
+    return config
+
+
+def generate_filename():
+    timestamp = str(int(time.time()))
+    return timestamp
+
+
+def set_default_setting(settings, config):
+    settings.update(
+        {
+            'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+            'LOG_LEVEL': 'WARNING',
+            'LOG_FILE': 'log.txt',
+            'FEED_EXPORT_ENCODING': 'utf-8',
+            'DOWNLOADER_MIDDLEWARES': {
+                'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+                'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+            }
+        }
+    )
+    if config['output_file_format'] in ['csv', 'json']:
+        filetype = config['output_file_format']
+        settings.update(
+            {
+                'FEED_FORMAT': filetype,
+                'FEED_URI': f'data/{generate_filename()}.{filetype}'
+            }
+        )
+    if config['output_file_format'] in ['excel', 'xlsx']:
+        settings.update(
+            {
+                'FEED_FORMAT': 'json',
+                'FEED_URI': f'data/{generate_filename()}.json'
+            }
+        )
+    return settings
+
+
+def export_to_excel(settings):
+    filename_raw = settings['FEED_URI']
+    filename = filename_raw[0:filename_raw.index('.')]
+
+    excel_filename = f'{filename}.xlsx'
+    export(filename_raw, excel_filename)
+    os.remove(filename_raw)
+    return excel_filename
+
+
 class PaginiauriiSpiderSpider(scrapy.Spider):
     name = 'paginiaurii_spider'
 
     base_urls = 'https://www.paginiaurii.ro'
+    output_file_format = 'default'
 
     def __init__(self, *a, **kw):
         super(PaginiauriiSpiderSpider, self).__init__(*a, **kw)
 
         self.start_urls = get_start_urls(self.search_query, self.search_location)
+
+    def closed(self, reason):
+        logging.warning('Exporting...')
+        filename = self.settings['FEED_URI']
+        if self.config['output_file_format'] in ['excel', 'xlsx']:
+            filename = export_to_excel(self.settings)
+
+        logging.warning('Sending an email...')
+        send_email(self.config['sender_email'], self.config['sender_password'], self.config['receiver_email'], filename)
+
+        logging.warning('Successfully finished.')
 
     def parse(self, response):
         logging.error(f'Parsing list: {response.url}')
@@ -46,6 +124,7 @@ class PaginiauriiSpiderSpider(scrapy.Spider):
 
     def parse_page(self, response):
         logging.error(f'Parsing page: {response.url}')
+
         def safe_strip(s: str) -> str:
             try:
                 return s.strip()
